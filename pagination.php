071e02df83dd1e5e9806fe2d3e266261a68f447e
mycodelobster <mycodelobster@outlook.com>
2014-03-29 22:39:29 +0700

    Update pagination.php

--- pagination.php
+++ pagination.php
@@ -18,7 +18,7 @@ $config['prev_link'] = '&lt;';
 $config['prev_tag_open'] = '<li>';
 $config['prev_tag_close'] = '</li>';
 
-$config['cur_tag_open'] = '<li class="bg-primary"><a><b>';
+$config['cur_tag_open'] = '<li><a><b>';
 $config['cur_tag_close'] = '</b></a></li>';
 
 
