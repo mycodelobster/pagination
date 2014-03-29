49d6fb23acc58cf2828bc3c6c647188e35fbac41
mycodelobster <mycodelobster@outlook.com>
2014-03-29 22:34:53 +0700

    Create pagination.php

---
+++ pagination.php
@@ -0,0 +1,28 @@
+<?php
+$config['full_tag_open'] = '<ul class="pagination pagination-small">';
+$config['full_tag_close'] = '</ul>';
+
+$config['first_link'] = 'First';
+$config['first_tag_open'] = '<li>';
+$config['first_tag_close'] = '</li>';
+
+$config['last_link'] = 'Last';
+$config['last_tag_open'] = '<li>';
+$config['last_tag_close'] = '</li>';
+
+$config['next_link'] = '&gt;';
+$config['next_tag_open'] = '<li>';
+$config['next_tag_close'] = '</li>';
+
+$config['prev_link'] = '&lt;';
+$config['prev_tag_open'] = '<li>';
+$config['prev_tag_close'] = '</li>';
+
+$config['cur_tag_open'] = '<li class="bg-primary"><a><b>';
+$config['cur_tag_close'] = '</b></a></li>';
+
+
+$config['num_tag_open'] = '<li>';
+$config['num_tag_close'] = '</li>';
+
+?>
